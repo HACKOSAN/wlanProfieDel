83e774df01da144a1da6e3058233756da06ac716
Hacko <76522226+HACKOSAN@users.noreply.github.com>
2023-02-28 21:57:13 +0300

    dirs

---
+++ wlanProfileDel/wlanProfileDel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+    // its frickin 2:47 AM, srlsy you should start paying me..
+
+Process process = new Process();
+process.StartInfo.FileName = "netsh.exe";
+process.StartInfo.Arguments = "wlan delete profile name=*";
+process.StartInfo.UseShellExecute = false;
+process.StartInfo.RedirectStandardOutput = true;
+process.Start();
+
+string output = process.StandardOutput.ReadToEnd();
+    process.WaitForExit();
+
+
